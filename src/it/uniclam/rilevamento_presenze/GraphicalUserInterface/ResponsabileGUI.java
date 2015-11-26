5e2207ad70f991c6f0935e66f7464f31daf99637
Chris7X <chris.xp@hotmail.it>
2015-11-26 23:17:14 +0100

    Aggiornato DBMS Data -ToString- # Conflicts: #	.idea/dataSources.xml

--- src/it/uniclam/rilevamento_presenze/GraphicalUserInterface/ResponsabileGUI.java
+++ src/it/uniclam/rilevamento_presenze/GraphicalUserInterface/ResponsabileGUI.java
@@ -3,5 +3,99 @@ package it.uniclam.rilevamento_presenze.GraphicalUserInterface;
 /**
  * Created by Chriz 7X on 26/11/2015.
  */
-public class ResponsabileGUI {
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+
+public class ResponsabileGUI extends JFrame {
+
+    public ResponsabileGUI() {
+
+        initUI();
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                ResponsabileGUI ex = new ResponsabileGUI();
+                ex.setVisible(true);
+            }
+        });
+    }
+
+    private void initUI() {
+
+        createMenuBar();
+
+        setTitle("Submenu");
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void createMenuBar() {
+
+        JMenuBar menubar = new JMenuBar();
+
+        ImageIcon iconNew = new ImageIcon("new.png");
+        ImageIcon iconOpen = new ImageIcon("open.png");
+        ImageIcon iconSave = new ImageIcon("save.png");
+        ImageIcon iconExit = new ImageIcon("exit.png");
+
+        JMenu fileMenu = new JMenu("File");
+
+        JMenu impMenu = new JMenu("Import");
+
+        //Secondo Menu'
+        JMenu viewMenu = new JMenu("View");
+        viewMenu.setMnemonic(KeyEvent.VK_V);
+
+
+        JMenuItem newsfMi = new JMenuItem("Import newsfeed list...");
+        JMenuItem bookmMi = new JMenuItem("Import bookmarks...");
+        JMenuItem mailMi = new JMenuItem("Import mail...");
+
+        impMenu.add(newsfMi);
+        impMenu.add(bookmMi);
+        impMenu.add(mailMi);
+
+        JMenuItem newMi = new JMenuItem("New", iconNew);
+        JMenuItem openMi = new JMenuItem("Open", iconOpen);
+        JMenuItem saveMi = new JMenuItem("Save", iconSave);
+
+        JMenuItem exitMi = new JMenuItem("Exit", iconExit);
+        exitMi.setToolTipText("Exit application");
+        openMi.setToolTipText("Nuovo");
+
+
+        exitMi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.exit(0);
+            }
+        });
+
+        fileMenu.add(newMi);
+        fileMenu.add(openMi);
+        fileMenu.add(saveMi);
+        fileMenu.addSeparator();
+        fileMenu.add(impMenu);
+        fileMenu.addSeparator();
+        fileMenu.add(exitMi);
+
+        menubar.add(fileMenu);
+
+        setJMenuBar(menubar);
+
+        //Secondo file
+        viewMenu.add(newMi);
+        menubar.add(viewMenu);
+    }
 }
